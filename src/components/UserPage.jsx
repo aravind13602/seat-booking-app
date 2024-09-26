453c3ffbeb3bfcb6d76e54eb37e06f244c926a19
aravind13602 <153479464+aravind13602@users.noreply.github.com>
2024-09-26 15:16:54 +0530

    88%

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './userPage.css';
+import './UserPage.css';
 
 const UserPage = ({ onLogout }) => {
   const [userSeats, setUserSeats] = useState([]);
